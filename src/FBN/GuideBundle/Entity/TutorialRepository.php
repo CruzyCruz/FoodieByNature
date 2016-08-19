bf625f88670d9f11eed58e296ccbbb837cdb54c6
Cédric Bonnin <bonnin.cedric@gmail.com>
2016-08-19 08:58:56 +0200

    Add Tutorial to backend : get article by translated slug

--- src/FBN/GuideBundle/Entity/TutorialRepository.php
+++ src/FBN/GuideBundle/Entity/TutorialRepository.php
@@ -15,16 +15,16 @@ class TutorialRepository extends EntityRepository
     public function getArticlesImages($first = 0, $limit = Article::NUM_ITEMS)
     {
         $qb = $this->createQueryBuilder('t')
-                   ->leftJoin('t.image', 'i')
-                   ->addSelect('i')
-                   ->orderBy('t.datePublication', 'DESC')
-                    ->where('t.publication = :publication')
-                    ->setParameter('publication', 1);
+            ->leftJoin('t.image', 'i')
+            ->addSelect('i')
+            ->orderBy('t.datePublication', 'DESC')
+            ->where('t.publication = :publication')
+            ->setParameter('publication', 1);
 
         $query = $qb->getQuery();
 
         $query->setFirstResult($first)
-              ->setMaxResults($limit);
+            ->setMaxResults($limit);
 
         return $query->getResult();
     }
@@ -32,18 +32,24 @@ class TutorialRepository extends EntityRepository
     public function getTutorial($slug)
     {
         $qb = $this->createQueryBuilder('t')
-                   ->leftJoin('t.image', 'i')
-                   ->addSelect('i')
-                   ->leftJoin('t.tutorialSection', 'tr')
-                   ->addSelect('tr')
-                   ->leftJoin('t.tutorialChapter', 'tc')
-                   ->addSelect('tc')
-                   ->leftJoin('tc.tutorialChapterParas', 'tcp')
-                   ->addSelect('tcp')
-                    ->where('t.slug = :slug')
-                    ->setParameter('slug', $slug);
+            ->leftJoin('t.image', 'i')
+            ->addSelect('i')
+            ->leftJoin('t.tutorialSection', 'tr')
+            ->addSelect('tr')
+            ->leftJoin('t.tutorialChapter', 'tc')
+            ->addSelect('tc')
+            ->leftJoin('tc.tutorialChapterParas', 'tcp')
+            ->addSelect('tcp')
+            ->where('t.slug = :slug')
+            ->setParameter('slug', $slug);
 
-        return $qb->getQuery()
-                  ->getOneOrNullResult();
+        $query = $qb->getQuery();
+
+        $query->setHint(
+            \Doctrine\ORM\Query::HINT_CUSTOM_OUTPUT_WALKER,
+            'Gedmo\\Translatable\\Query\\TreeWalker\\TranslationWalker'
+        );
+
+        return $query->getOneOrNullResult();
     }
 }
