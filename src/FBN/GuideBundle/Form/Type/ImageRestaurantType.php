3eac1b64cd24d52ed6ac8c262647c43fc1f84f55
Cruzy Cruz <bonnin.cedric@gmail.com>
2017-05-13 11:58:51 +0200

    Remove duplicated code in image types

--- src/FBN/GuideBundle/Form/Type/ImageRestaurantType.php
+++ src/FBN/GuideBundle/Form/Type/ImageRestaurantType.php
@@ -2,41 +2,10 @@
 
 namespace FBN\GuideBundle\Form\Type;
 
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ImageRestaurantType extends ImageType
 {
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array                $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $masterRequest = $this->requestStack->getMasterRequest();
-
-        $requiredFile = $this->formManager->isFileFieldRequired($masterRequest);
-
-        $builder
-            ->add('legend', TextType::class, array(
-                'required' => true,
-                ))
-            ->add('file', VichImageType::class, array(
-                'required' => $requiredFile,
-                'allow_delete' => false, // not mandatory, default is true
-                'download_link' => true, // not mandatory, default is true
-                ))
-        ;
-
-        $this->formManager->disableNonTranslatableFormFieldsForNonDefaultLocale(
-            $builder,
-            self::$fieldsToBeDisabled,
-            $masterRequest->getLocale())
-        ;
-    }
-
     /**
      * @param OptionsResolverInterface $resolver
      */
--- src/FBN/GuideBundle/Form/Type/ImageType.php
+++ src/FBN/GuideBundle/Form/Type/ImageType.php
@@ -2,8 +2,11 @@
 
 namespace FBN\GuideBundle\Form\Type;
 
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use FBN\GuideBundle\Form\Manager\FormManager;
 
 class ImageType extends AbstractType
@@ -26,5 +29,33 @@ class ImageType extends AbstractType
     {
         $this->requestStack = $requestStack;
         $this->formManager = $formManager;
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $masterRequest = $this->requestStack->getMasterRequest();
+
+        $requiredFile = $this->formManager->isFileFieldRequired($masterRequest);
+
+        $builder
+            ->add('legend', TextType::class, array(
+                'required' => true,
+                ))
+            ->add('file', VichImageType::class, array(
+                'required' => $requiredFile,
+                'allow_delete' => false, // not mandatory, default is true
+                'download_link' => true, // not mandatory, default is true
+                ))
+        ;
+
+        $this->formManager->disableNonTranslatableFormFieldsForNonDefaultLocale(
+            $builder,
+            self::$fieldsToBeDisabled,
+            $masterRequest->getLocale())
+        ;
     }
 }
